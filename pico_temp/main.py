e672ef32689e8686e81550d8d215c6cfc92577c4
Mike Dalrymple <mike@mousedown.com>
2022-07-23 15:44:12 -0700

    added ones output

--- pico_temp/main.py
+++ pico_temp/main.py
@@ -9,13 +9,23 @@ conversion_factor = 3.3 / 65535
 
 
 def flash_temp(temp):
-    count_left = temp / 10
-    while count_left > 0:
+    tens_left = temp / 10
+    ones_left = temp % 10
+    while tens_left > 0:
+        led_external(1)
+        time.sleep_ms(200)
+        led_external(0)
+        time.sleep_ms(200)
+        tens_left = tens_left - 1
+    led(1)
+    time.sleep_ms(500)
+    led(0)
+    while ones_left > 0:
         led_external(1)
         time.sleep_ms(500)
         led_external(0)
         time.sleep_ms(500)
-        count_left = count_left - 1
+        ones_left = ones_left - 1
 
 
 while True:
